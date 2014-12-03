4374b5ba42e9d4dc9aceb50c92d3a387c267a8b1
Weijian Zhang <weijian.z@hotmail.com>
2014-12-03 12:06:53 +0000

    minor change

--- test/test_magic.jl
+++ test/test_magic.jl
@@ -1,12 +1,14 @@
 n = rand(1:10)
 M = matrixdepot("magic", n)
-if n == 2 # no magic square of order 2 
-    M = matrixdepot("magic", 3)
+if n == 2 # no magic square of order 2
+    return
+else
     @test sum(M,1) == sum(M,2)'
     k = rand(1:n)
     @test sum(M,1)[k] == sum(M,2)'[k]
+    # diagnoal == antidiagnoal
+    p = [n:-1:1]
+    @test sum(diag(M)) == sum(diag(M[:,p]))
 end
-# diagnoal == antidiagnoal
-p = [n:-1:1]
-@test sum(diag(M)) == sum(diag(M[:,p]))
+
 println("'magic' passed test...")
