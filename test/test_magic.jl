fe04f977e2970fd94ea25e275b879a951a404f53
Weijian Zhang <weijian.z@hotmail.com>
2014-12-03 12:01:17 +0000

    fix error with magic matrix

--- test/test_magic.jl
+++ test/test_magic.jl
@@ -1,10 +1,11 @@
 n = rand(1:10)
 M = matrixdepot("magic", n)
-if n != 2 # no magic matrix of order 2
+if n == 2 # no magic square of order 2 
+    n = 3
     @test sum(M,1) == sum(M,2)'
+    k = rand(1:n)
+    @test sum(M,1)[k] == sum(M,2)'[k]
 end
-k = rand(1:n)
-@test sum(M,1)[k] == sum(M,2)'[k]
 # diagnoal == antidiagnoal
 p = [n:-1:1]
 @test sum(diag(M)) == sum(diag(M[:,p]))
