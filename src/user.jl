efe39c55293168d6977e4550310f9ed2b81dd8c5
Weijian Zhang <weijian.z@hotmail.com>
2014-12-16 22:23:39 +0000

    initial user.jl

--- src/user.jl
+++ src/user.jl
@@ -1,11 +1,5 @@
-usermatrixclass =
+usermatrixclass = 
 @compat Dict(
 
-
-
-
-"newclass" => ["cauchy", "hilb", ],
-
-"spd" => ["hilb", "cauchy", "circul", "invhilb", "moler", "pascal", "pei", "minij", "tridiag", "lehmer", ],
 
 );
