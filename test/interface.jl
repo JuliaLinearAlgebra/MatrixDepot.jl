b2eeb96042aeb13833118a65ca5b182c242bd086
Weijian Zhang <weijian.z@hotmail.com>
2015-03-10 19:39:02 +0000

    update test for interfaces

---
+++ test/interface.jl
@@ -0,0 +1,9 @@
+matrixdepot("illc1850")
+A = matrixdepot("illc1850", :r)
+
+println("UF sparse matrix collection interface passed test...")
+
+matrixdepot("685_bus")
+B = matrixdepot("685_bus", :r)
+
+println("Matrix Market interface passed test...")
