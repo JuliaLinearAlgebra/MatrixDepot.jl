b5ab701b959d252b278b7560a77d537b3cababfe
Weijian Zhang <weijian.z@hotmail.com>
2015-04-19 10:57:39 +0100

    add references for test matrices

--- src/higham.jl
+++ src/higham.jl
@@ -832,26 +832,31 @@ matrixinfo =
              \n [type,] dim: the dimension of the matrix
              \n [type,] row_dim, col_dim: the row and column dimension 
              \n ['inverse', 'ill-cond', 'symmetric', 'pos-def']",
+
              "invhilb" => "Inverse of Hilbert matrix:
              \n Input options:
              \n [type,] dim: the dimension of the matrix
              \n ['inverse', 'ill-cond', 'symmetric','pos-def']",
+
              "hadamard" => "Hadamard matrix: 
              \n Input options: 
              \n [type,] dim: the dimension of the matrix, n is a power of 2 
              \n ['inverse', 'orthogonal', 'eigen']",
+
              "cauchy" => "Cauchy matrix: 
              \n Input options: 
              \n vec1, vec2: two vectors 
              \n vec: a vector
              \n [type,] dim: the dimension of the matrix
              \n ['inverse', 'ill-cond', 'symmetric', 'pos-def']",
+
              "circul" => "Circul matrix: 
              \n Input options: 
              \n vec, n: a vector and the column dimension 
              \n vec: a vector 
              \n [type,] dim: the dimension of the matrix
              \n ['symmetric', 'pos-def', 'eigen']",
+
              "dingdong" => "Dingdong matrix:
              \n Input options:
              \n [type,] n: the dimension of the matrix.
@@ -859,22 +864,26 @@ matrixinfo =
              \n Reference: J.C. Nash, Compact Numerical Methods for 
              Computers: Linear Algebra and Function Minimisation, 
              second edition, Adam Hilger, Bristol, 1990 (Appendix 1).",
+
              "frank" => "Frank matrix:
              \n Input options:
              \n [type,] n, k: n is the dimension of the matrix, k = 0 or 1.
              If k = 1 the matrix reflect about the anti-diagonal.
              \n [type,] n: n is the dimension of the matrix.
              \n ['ill-cond', 'eigen']",
+
              "forsythe" => "Forsythe matrix:
              \n Input options:
              \n [type,] n, alpha, lambda: n is the dimension of the matrix.
              alpha and lambda are scalars.
              \n [type,] n: alpha = sqrt(eps(type)) and lambda = 0.
              \n ['inverse', 'ill-cond', 'eigen']",
+
              "magic" => "Magic square matrix:
              \n Input options:
              \n [type,] dim: the dimension of the matrix.
              \n ['inverse']",
+
              "grcar" => "Grcar Matrix:
              \n Input options:
              \n [type,] dim, k: dim is the dimension of the matrix and 
@@ -884,6 +893,7 @@ matrixinfo =
              \n Reference: J.F. Grcar, Operator coefficient methods 
              for linear equations, Report SAND89-8691, Sandia National 
              Laboratories, Albuquerque, New Mexico, 1989 (Appendix 2).",
+
              "triw" => "Triw Matrix:
              \n Input options:
              \n [type,] row_dim, col_dim, alpha, k: row_dim and col_dim 
@@ -892,69 +902,91 @@ matrixinfo =
              k is the number superdiagonals.
              \n [type,] dim 
              \n ['inverse', 'ill-cond']",
+
              "moler" => "Moler Matrix:
              \n Input options:
              \n [type,] dim, alpha: dim is the dimension of the matrix,
              alpha is a scalar.
              \n [type,] dim: alpha = -1.
              \n ['inverse', 'ill-cond', 'symmetric', 'pos-def']",
+
              "pascal" => "Pascal Matrix:
              \n Input options:
              \n [type,] dim: the dimension of the matrix.
              \n ['Inverse', 'ill-cond', 'symmetric', 'pos-def', 'eigen']",
+
              "kahan" => "Kahan Matrix:
              \n Input options:
              \n [type,] m, n, theta, pert: m, n are the row and column 
              dimensions of the matrix. theta and pert are scalars.
              \n [type,] dim, theta, pert: dim is the dimension of the matrix.
              \n [type,] dim: theta = 1.2, pert = 25.
-             \n ['inverse', 'ill-cond']" ,
+             \n ['inverse', 'ill-cond']
+             \n Reference: W. Kahan, Numerical linear algebra, Canadian Math.
+             Bulletin, 9 (1966), pp. 757-801." ,
+
              "pei" => "Pei Matrix:
              \n Input options:
              \n [type,] dim, alpha: dim is the dimension of the matrix.
              alpha is a scalar.
              \n [type,] dim
-             \n ['inverse', 'ill-cond', 'symmetric', 'pos-def']",
+             \n ['inverse', 'ill-cond', 'symmetric', 'pos-def']
+             \n Reference: M.L. Pei, A test matrix for inversion procedures,
+             Comm. ACM, 5 (1962), p. 508.",
+
              "vand" => "Vandermonde Matrix:
              \n Input options:
              \n vec, dim: vec is a vector, dim is the number of columns.
              \n vec
              \n [type,] dim
-             \n ['inverse', 'ill-cond']",
+             \n ['inverse', 'ill-cond']
+             \n Reference: N.J. Higham, Stability analysis of algorithms 
+             for solving confluent Vandermonde-like systems, SIAM J. 
+             Matrix Anal. Appl., 11 (1990), pp. 23-41.",
+
              "invol" => "Involutory Matrix:
              \n Input options:
              \n [type,] dim: dim is the dimension of the matrix.
              \n ['inverse', 'ill-cond', 'eigen']",
+
              "chebspec" => "Chebyshev spectral differentiation matrix:
              \n Input options:
              \n [type,] dim, k: dim is the dimension of the matrix and 
              k = 0 or 1.
              \n [type,] dim
              \n ['eigen']",
+
              "lotkin" => "Lotkin Matrix:
              \n Input options:
              \n [type,] dim: dim is the dimension of the matrix.
              \n ['inverse', 'ill-cond', 'eigen']",
+
              "clement" => "Clement Matrix:
              \n Input options:
              \n [type,] dim, k: dim is the dimension of the matrix. 
              If k = 0, the matrix is Tridiagonal. If k = 1, the matrix 
              is SymTridiagonal.
              \n [type,] dim: k = 0
-             \n ['inverse', 'symmetric', 'eigen']",
+             \n ['inverse', 'symmetric', 'eigen']
+             \n Reference: P.A. Clement, A class of triple-diagonal 
+             matrices for test purposes, SIAM Review, 1 (1959), pp. 50-52.",
+
              "fiedler" => "Fiedler Matrix:
              \n Input options:
              \n vec: vec is a vector.
              \n [type,] dim: dim is the dimension of the matrix.
              \n ['inverse', 'symmetric', 'eigen']",
+
              "minij" => "MIN[I,J] Matrix:
              \n Input options:
              \n [type,] dim: dim is the dimension of the matrix.
              \n ['inverse', 'symmetric', 'pos-def', 'eigen']",
+
              "binomial" => "Binomial Matrix:
              \n Input options:
              \n [type,] dim: dim is the dimension of the matrix.
              \n ['']",
+
              "tridiag" => "Tridiagonal Matrix:
              \n Input options:
              \n v1, v2, v3: v1 and v3 are sub- superdiagonal vectors.
@@ -963,33 +995,56 @@ matrixinfo =
              scalars. x and z are sub- superdiagonal elements, y is diagonal 
              element.
              \n [type,] dim: x = -1, y = 2, z = -1.
-             \n ['inverse', 'ill-cond', 'pos-def', 'eigen']",
+             \n ['inverse', 'ill-cond', 'pos-def', 'eigen']
+             \n Reference: J. Todd, Basic Numerical Mathematics, Vol. 2:
+             Numerical Algebra, Birkhauser, Basel, and Academic Press,
+             New York, 1977, p. 155.",
+
              "lehmer" => "Lehmer Matrix:
              \n Input options: 
              \n [type,] dim: the dimension of the matrix.
-             \n ['inverse', 'symmetric', 'pos-def']",
+             \n ['inverse', 'symmetric', 'pos-def']
+             \n Reference: M. Newman and J. Todd, The evaluation of 
+             matrix inversion programs, J. Soc. Indust. Appl. Math., 
+             6 (1958), pp. 466-476. 
+             Solutions to problem E710 (proposed by D.H. Lehmer): The inverse
+             of a matrix, Amer. Math. Monthly, 53 (1946), pp. 534-535.",
+
              "parter" => "Parter Matrix:
              \n Input options:
              \n [type,] dim: the dimension of the matrix.
-             \n ['eigen']",
+             \n ['eigen']
+             \n Reference: The MathWorks Newsletter, Volume 1, Issue 1,
+             March 1986, page 2. S.V. Parter, On the distribution of the
+             singular values of Toeplitz matrices, Linear Algebra and 
+             Appl., 80 (1986), pp. 115-130.",
+
              "chow" => "Chow Matrix:
              \n Input options:
              \n [type,] dim, alpha, delta: dim is dimension of the matrix. 
              alpha, delta are scalars such that A[i,i] = alpha + delta and 
              A[i,j] = alpha^(i + 1 -j) for j + 1 <= i.
              \n [type,] dim: alpha = 1, delta = 0.
-             \n ['eigen']",
+             \n ['eigen']
+             \n Reference: T.S. Chow, A class of Hessenberg matrices with known
+             eigenvalues and inverses, SIAM Review, 11 (1969), pp. 391-395.",
+
              "randcorr" => "Random Correlation Matrix:
              \n Input options:
              \n [type,] dim: the dimension of the matrix.
              \n ['symmetric', 'pos-semidef', 'random']",
+
              "poisson" => "Poisson Matrix:
              \n Input options:
              \n [type,] n: the dimension of the matirx is n^2.
              \n ['inverse', 'symmetric', 'pos-def', 'eigen', 'sparse']",
+
              "neumann" => "Neumann Matrix:
              \n [type,] n: the dimension of the matrix is n^2.
-             \n ['eigen', 'sparse']",
+             \n ['eigen', 'sparse']
+             \n Reference: R.J. Plemmons, Regular splittings and the 
+             discrete Neumann problem, Numer. Math., 25 (1976), pp. 153-161.",
+
              "rosser" => "Rosser Matrix:
              \n Input options:
              \n [type,] dim, a, b: dim is the dimension of the matrix.
@@ -997,16 +1052,26 @@ matrixinfo =
              a and b are scalars. For dim = 8, a = 2 and b = 1, the generated 
              matrix is the test matrix used by Rosser.
              \n [type,] dim: a = b = rand(1:5)
-             \n ['eigen', 'ill-cond', 'random']",
+             \n ['eigen', 'ill-cond', 'random']
+             \n Reference: J.B. Rosser, C. Lanczos, M.R. Hestenes, W. Karush,
+             Separation of close eigenvalues of a real symmetric matrix, 
+             Journal of Research of the National Bureau of Standards, v(47)
+             (1951)",
+
              "sampling" => "Matrix with Application in Sampling Theory:
              \n Input options:
              \n vec: vec is a vector with no repeated elements.
              \n [type,] dim: the dimension of the matrix. 
-             \n ['eigen']",
+             \n ['eigen']
+             \n Reference: L. Bondesson and I. Traat, A nonsymmetric matrix
+             with integer eigenvalues, linear and multilinear algebra, 55(3)
+             (2007), pp. 239-247",
+
              "wilkinson" => "Wilkinson Matrix:
              \n Input options:
              \n [type,] dim: the dimension of the matrix.
              \n ['symmetric', 'eigen']",
+
              "rando" => "Random Matrix with Element -1, 0, 1:
              \n Input options:
              \n [type,] m, n, k: m and n are row and column dimensions, 
@@ -1016,6 +1081,7 @@ matrixinfo =
              \n [type,] n, k
              \n [type,] n: k = 1
              \n ['random']",
+
              "randsvd" => "Random Matrix with Pre-assigned Singular Values:
              \n Input options:
              \n [type,] m, n, kappa, mode: m, n are the dimensions of the matrix.
@@ -1028,21 +1094,31 @@ matrixinfo =
              \n [type,] n, kappa, mode: m = n
              \n [type,] n, kappa: mode = 3
              \n [type,] n: kappa = sqrt(1/eps()), mode = 3.
-             \n ['ill-cond', 'random']",
+             \n ['ill-cond', 'random']
+             \n Reference: N.J. Higham, Accuracy and Stability of Numerical 
+             Algorithms, Society for Industrial and Applied Mathematics, 
+             Philadelphia, PA, USA, 1996; sec. 26.3.",
+
              "rohess" => "Random Orthogonal Upper Hessenberg Matrix:
              \n Input options:
              \n [type,] n : n is the dimension of the matrix.
              \n ['random']",
+
              "kms" => "Kac-Murdock-Szego Toeplitz Matrix:
              \n [type,] n, rho: n is the dimension of the matrix, rho is a 
              scalar such that A[i,j] = rho^(abs(i-j)).
              \n [type,] n: rho = 0.5
              \n ['inverse', 'ill-cond', 'symmetric', 'pos-def']",
+
              "wathen" => "Wathen Matrix:
              \n [type,] nx, ny: the dimension of the matrix is equal to
              3 * nx * ny + 2 * nx * ny + 1.
              \n [type,] n: nx = ny = n. 
-             \n ['symmetric', 'pos-def', 'eigen', 'random', 'sparse']"
+             \n ['symmetric', 'pos-def', 'eigen', 'random', 'sparse']
+             \n Reference: A.J. Wathen, Realistic eigenvalue bounds for 
+             the Galerkin mass matrix, IMA J. Numer. Anal., 7 (1987), 
+             pp. 449-457."
+
              );
 
 matrixclass = 
