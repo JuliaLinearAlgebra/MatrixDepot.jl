f0b94ea206877e7311978ec2ef0b3d939e11ed8b
Weijian Zhang <weijian.z@hotmail.com>
2015-04-20 21:38:03 +0100

    initialize regularization test matrices

---
+++ src/regu.jl
@@ -0,0 +1,17 @@
+# Test matrices for regularization methods from Hansen's 
+# Regularization toolbox
+# 
+# Per Christian Hansen, Test matrices for regularization methods
+# SIAM J. SCI. COMPUT Vol 16, No2, pp 506-512, (1995)
+
+
+immutable RegProb{T}
+    A::Matrix{T}  # matrix of interest
+    b::Vector{T}  # right-hand side
+    x::Vector{T}  # the solution to Ax = b
+end
+
+
+function baart{T}(::Type{T}, n::Int)
+    
+end
