2d2c84bed784008a0113ec24e05fd1046eaec818
Weijian Zhang <weijian.z@hotmail.com>
2015-08-14 18:37:16 +0100

    initialize heat

--- src/regu.jl
+++ src/regu.jl
@@ -1,12 +1,6 @@
 # Test matrices for regularization methods from Hansen's
 # Regularization toolbox
 
-immutable RegProb{T}
-    A::Matrix{T}  # matrix of interest
-    b::Vector{T}  # right-hand side
-    x::Vector{T}  # the solution to Ax = b
-end
-
 # compute a oscillating matrix from a given signular value spectrum
 # using the idea proposed by Per Christian Hansen, "Test matrices for
 # regularization methods. 
@@ -19,27 +13,17 @@ function oscillate{T}(Σ::Vector{T})
     return U*diagm(Σ)*U'
 end
         
-#  mode = 1: one large singular value.
-#  mode = 2: one small singular value.
-#  mode = 3: geometrically distributed singular values.
-#  mode = 4: arithmetrically distributed singular values.
-#  mode = 5: random singular values with  unif. dist. logarithm.
+
+#  mode = 1: geometrically distributed singular values.
+#  mode = 2: arithmetrically distributed singular values.
 #  κ = sqrt(1/eps(T)) is the condition number of the matrix.
 function oscillate{T}(::Type{T}, n::Int, mode::Int)
     κ = sqrt(1/eps(T))
-    if mode == 3
+    if mode == 1
         factor = κ^(-1/(n-1))
         Σ = factor.^[0:n-1;]
-    elseif mode == 4
-        Σ = ones(T, n) - T[0:n-1;]/(n-1)*(1 - 1/κ)
-    elseif mode == 5
-        Σ = exp(-rand(n) * log(κ))
     elseif mode == 2
-        Σ = ones(T, n)
-        Σ[n] = one(T)/κ
-    elseif mode == 1
-        Σ = ones(n)./κ
-        Σ[1] = one(T)
+        Σ = ones(T, n) - T[0:n-1;]/(n-1)*(1 - 1/κ)
     else
         error("invalid mode value.")
     end
@@ -47,3 +31,42 @@ function oscillate{T}(::Type{T}, n::Int, mode::Int)
 end
 oscillate{T}(::Type{T}, n::Int) = oscillate(T, n, 3)
 
+
+immutable RegProb{T}
+    A::Matrix{T}  # matrix of interest
+    b::Vector{T}  # right-hand side
+    x::Vector{T}  # the solution to Ax = b
+end
+
+# The following test problems are derived from Per Christian Hansen's
+# Regularization tools for MATLAB. 
+# http://www.mathworks.com/matlabcentral/fileexchange/view_license?file_info_id=52
+#
+# Copyright (c) 2015, Per Christian Hansen
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the distribution
+#     * Neither the name of the DTU Compute nor the names
+#       of its contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+function heat{T}(::Type{T}, n::Int, κ::Real, compute_bx::Bool = false)
+    
+end
