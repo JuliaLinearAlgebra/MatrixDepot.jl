57f92f82deb59ea9cfe00d758bf2dc8098d0e7e7
Weijian Zhang <weijian.z@hotmail.com>
2015-08-14 07:35:13 +0100

    update test and add documentation for prolate and toeplitz matrix

--- test/test_toeplitz.jl
+++ test/test_toeplitz.jl
@@ -1,9 +1,9 @@
-A = matrixdepot("toeplitz" [1,2,3,4,5])
+A = matrixdepot("toeplitz", [1,2,3,4,5])
 @test A == matrixdepot("toeplitz", 5)
 
 B = matrixdepot("toeplitz", [1,2,3], [1,4,5])
-@test B[3,3] == 5
-@test B[1,3] == 3
+@test B[1,3] == 5
+@test B[3,1] == 3
 @test B[1,2] == 4
 println("'toeplitz' passed test...")
 
