6441b59a7fe5c5f7612c350c8878ac0db3cef6f9
Weijian Zhang <weijian.z@hotmail.com>
2015-09-17 21:27:16 +0100

    erase user data

--- src/user.jl
+++ src/user.jl
@@ -2,7 +2,5 @@ usermatrixclass =
 @compat Dict(
 
 
-
-"mysymmtric" => ["hilb", "cauchy", "circul", "dingdong", "invhilb", "moler", "pascal", "pei", "clement", "fiedler", "minij", "tridiag", "lehmer", "randcorr", "poisson", "wilkinson", "kms", "wathen", "oscillate", "prolate", "deriv2", "shaw", ],
 );
 
